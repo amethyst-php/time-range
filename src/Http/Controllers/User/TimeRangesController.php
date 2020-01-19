11d38183bebcda190a10dc2b689bbc084bf4ed6c
railken <railken@protonmail.ch>
2020-01-19 18:32:11 +0100

    fix namespaces

--- src/Http/Controllers/User/TimeRangesController.php
+++ src/Http/Controllers/User/TimeRangesController.php
@@ -2,8 +2,8 @@
 
 namespace Amethyst\Http\Controllers\User;
 
-use Amethyst\Api\Http\Controllers\RestManagerController;
-use Amethyst\Api\Http\Controllers\Traits as RestTraits;
+use Amethyst\Core\Http\Controllers\RestManagerController;
+use Amethyst\Core\Http\Controllers\Traits as RestTraits;
 use Amethyst\Managers\TimeRangeManager;
 use Illuminate\Http\Request;
 
